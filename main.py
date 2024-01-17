8ee10ac642f3068b7ce7197d7309ea25f2a42d50
Lucas Maciel Gomes <lmg.snf23@uea.edu.br>
2024-01-16 22:16:35 -0400

    missiles

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 from random import randint
-
 pygame.init()
 
 FPS = 60
@@ -25,7 +24,7 @@ pygame.display.set_caption("Galactic Intruders")
 
 class Player():
     COLOR = WHITE
-    VEL = 3
+    VEL = 5
 
     def __init__(self, x, y, width, height):
         self.x, self.y = x, y
@@ -52,7 +51,7 @@ class Player():
 
     def explode(self):
         current_time = pygame.time.get_ticks()
-        if current_time - self.last_explosion_time > 1000:
+        if current_time - self.last_explosion_time > 800:
             new_explosion = Explosion(self.x + self.width // 2, self.y + self.height // 2, EXPLOSION_RADIUS, EXPLOSION_MAX_RADIUS)
             self.explosions.append(new_explosion)
             self.last_explosion_time = current_time
@@ -88,23 +87,23 @@ class Explosion():
         return self.duration <= 0
 
 class Missile():
-    COLOR = (255, 165, 0)
     VEL = 1
 
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, cor):
         self.x, self.y = x, y
         self.width, self.height = 10, 10
+        self.color = cor
 
     def draw(self, win):
-        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
 
     def move(self, down=True):
         self.y += self.VEL if down else 0
 
-def generate_missile():
+def generate_missile(cor):
     x = randint(0, WIDTH - 10)
     y = 0
-    return Missile(x, y, 10, 10)
+    return Missile(x, y, 10, 10, cor)
 
 def draw_missiles(win, missiles):
     for missile in missiles:
@@ -126,10 +125,6 @@ def movement(keys, player):
         player.explode()
 
 def main():
-    pygame.mixer.music.load("assets/i_wonder.wav")
-    pygame.mixer.music.set_volume(0.3)
-    pygame.mixer.music.play(-1)
-
     game_loop = True
     clock = pygame.time.Clock()
 
@@ -138,6 +133,12 @@ def main():
     
     missiles = []
 
+    horda = 1
+    inimigo_chance_spawn = {'verde': 50, 'amarelo': 50, 'laranja': 0, 'vermelho': 0}
+    inimigo_velocidade_buff = 1.0
+    current_time = pygame.time.get_ticks()
+    last_spawn_time = pygame.time.get_ticks()
+    spawn_interval = 300
     while game_loop:
         clock.tick(FPS)
 
@@ -145,9 +146,51 @@ def main():
         draw_base(SCREEN, base)
         draw_player(SCREEN, player)
         draw_missiles(SCREEN, missiles)
+
+        current_time = pygame.time.get_ticks()
+        if horda % 2 == 1 and horda <= 20:
+            inimigos_por_horda = 50 + (horda - 1) * 20
+            while len(missiles) < inimigos_por_horda and current_time - last_spawn_time >= spawn_interval:
+                if randint(0, 100) < inimigo_chance_spawn['verde']:
+                    missiles.append(generate_missile(GREEN))
+                if randint(0, 100) < inimigo_chance_spawn['amarelo']:
+                    missiles.append(generate_missile(YELLOW))
+                last_spawn_time = current_time
         
-        if randint(0, 100) < 5:  
-            missiles.append(generate_missile())
+        elif horda % 2 == 0 and horda <= 20:
+            if horda >= 3 and horda <= 4:
+                inimigo_chance_spawn['laranja'] += 10
+                inimigo_chance_spawn['amarelo'] -= 10
+                inimigo_chance_spawn['verde'] += 10
+                spawn_interval -= 10
+            elif horda >= 5 and horda <= 6:
+                inimigo_velocidade_buff += 0.2
+                spawn_interval -= 10
+                inimigo_chance_spawn['laranja'] += 10
+                inimigo_chance_spawn['vermelho'] += 10
+                inimigo_chance_spawn['verde'] += 10
+            elif horda >= 7 and horda <= 8:
+                inimigo_chance_spawn['vermelho'] += 10
+                spawn_interval -= 10
+            elif horda >= 9 and horda <= 10:
+                inimigo_chance_spawn['amarelo'] -= 10
+                spawn_interval -= 10
+                inimigo_velocidade_buff += 0.2
+            elif horda >= 11 and horda <= 12:
+                inimigo_chance_spawn['laranja'] += 10
+                spawn_interval -= 10
+            elif horda >= 13 and horda <= 14:
+                inimigo_velocidade_buff += 0.2
+                spawn_interval -= 10
+            elif horda >= 15 and horda <= 16:
+                inimigo_chance_spawn['amarelo'] = 0
+                spawn_interval -= 10
+            elif horda >= 17 and horda <= 18:
+                inimigo_chance_spawn['vermelho'] += 10
+                spawn_interval -= 10
+            elif horda >= 19 and horda <= 20:
+                inimigo_velocidade_buff += 0.2
+                spawn_interval -= 10
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -167,6 +210,10 @@ def main():
 
         pygame.display.update()
 
+        if len(missiles) == 0 and horda <= 20:
+            horda += 1
+            print(f'Horda {horda}')
+            
     pygame.quit()
 
 if __name__ == '__main__':
---
+++ readme.md
@@ -0,0 +1,26 @@
+Horda 1 - 2:
+Apenas inimigos do tipo verde ou amarelo devem ser spawnados.
+Horda 3 - 4:
+Inimigos do tipo laranja começam a ser spawnados;
+Chance de spawn dos inimigos do tipo amarelo é diminuída;
+Chance de spawn dos inimigos do tipo verde é aumentada;
+Horda 5 - 6:
+Todos os tipos de inimigos têm um leve buff de velocidade;
+Todos os tipos de inimigos têm um leve buff na chance de spawn.
+Horda 7 - 8:
+Inimigos do tipo vermelho podem ser spawnados.
+Horda 9 - 10:
+Inimigos do tipo amarelo têm sua chance de spawn diminuída;
+Todos os tipos de inimigos têm um leve buff na velocidade.
+Horda 11 - 12:
+Limite de inimigos na tela pode ser aumentado;
+Inimigos do tipo laranja tem a chance de spawn aumentada.
+Horda 13 - 14:
+Todos os tipos de inimigos têm um leve buff de velocidade.
+Horda 15 - 16:
+Inimigos do tipo amarelo não surgem mais.
+Horda 17 - 18:
+Inimigos do tipo vermelho têm um buff na chance de spawn.
+Horda 19 - 20:
+Inimigos de todos os tipos recebem um buff de velocidade.
+
